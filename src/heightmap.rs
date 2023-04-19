8f60d16496ed27ea59ac746c691f071a1c7f218f
Pontus Asp <pontus.asp@gmail.com>
2023-04-19 22:36:16 +0200

    Split project into modules

---
+++ src/heightmap.rs
@@ -0,0 +1,72 @@
+
+#[derive(Debug)]
+pub struct Heightmap {
+    pub data: Vec<Vec<f32>>,
+    pub width: usize,
+    pub height: usize,
+    pub depth: f32
+}
+
+#[derive(Debug)]
+pub enum HeightmapError {
+    MismatchingSize
+}
+
+impl Heightmap {
+    pub fn new(data: Vec<Vec<f32>>, width: usize, height: usize, depth: f32) -> Heightmap {
+        Heightmap {
+            data,
+            width,
+            height,
+            depth
+        }
+    }
+
+    pub fn erode(&self) -> Heightmap {
+        let data = self.data.clone();
+        let width = self.width;
+        let height = self.height;
+        let depth = self.depth;
+        Heightmap::new(data, width, height, depth)
+    }
+
+    pub fn to_u8(&self) -> Vec<u8> {
+        let mut buffer: Vec<u8> = Vec::new();
+
+        for i in 0..self.width {
+            for j in 0..self.height {
+                let mut value = self.data[i][j];
+                let u8_max: f32 = 255.0;
+                value = value / (self.depth / u8_max);
+                value = value.round();
+                let value = value as i32;
+                
+                buffer.push(value.try_into().unwrap());
+            }
+        }
+
+        buffer
+    }
+
+    pub fn subtract(&self, heightmap: &Heightmap) -> Result<Heightmap, HeightmapError> {
+        let mut data: Vec<Vec<f32>> = Vec::new();
+        
+        if !(self.width == heightmap.width && self.height == heightmap.height) {
+            return Err(HeightmapError::MismatchingSize)
+        }
+
+        for i in 0..self.width {
+            let mut row = Vec::new();
+            for j in 0..self.height {
+                let value = self.data[i][j] - heightmap.data[i][j] + heightmap.depth;
+                row.push(value);
+            }
+            data.push(row);
+        }
+
+        let diff = Heightmap::new(data, self.width, self.height, self.depth + heightmap.depth);
+        Ok(diff)
+    }
+
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
 use ds_heightmap::Runner;
 
-#[derive(Debug)]
-struct Heightmap {
-    data: Vec<Vec<f32>>,
-    width: usize,
-    height: usize,
-    depth: f32
-}
+pub mod heightmap;
+pub mod erode;
 
-#[derive(Debug)]
-enum HeightmapError {
-    MismatchingSize
-}
-
-fn create_heightmap(size: usize, depth: f32, roughness: f32) -> Heightmap {
+fn create_heightmap(size: usize, depth: f32, roughness: f32) -> heightmap::Heightmap {
     let mut runner = Runner::new();
     runner.set_height(size);
     runner.set_width(size);
@@ -22,7 +12,7 @@ fn create_heightmap(size: usize, depth: f32, roughness: f32) -> Heightmap {
     runner.set_rough(roughness);
     
     let output = runner.ds();
-    Heightmap {
+    heightmap::Heightmap {
         data: output.data,
         width: size,
         height: size,
@@ -30,65 +20,7 @@ fn create_heightmap(size: usize, depth: f32, roughness: f32) -> Heightmap {
     }
 }
 
-impl Heightmap {
-    fn new(data: Vec<Vec<f32>>, width: usize, height: usize, depth: f32) -> Heightmap {
-        Heightmap {
-            data,
-            width,
-            height,
-            depth
-        }
-    }
-
-    fn erode(&self) -> Heightmap {
-        let data = self.data.clone();
-        let width = self.width;
-        let height = self.height;
-        let depth = self.depth;
-        Heightmap::new(data, width, height, depth)
-    }
-
-    fn to_u8(&self) -> Vec<u8> {
-        let mut buffer: Vec<u8> = Vec::new();
-
-        for i in 0..self.width {
-            for j in 0..self.height {
-                let mut value = self.data[i][j];
-                let u8_max: f32 = 255.0;
-                value = value / (self.depth / u8_max);
-                value = value.round();
-                let value = value as i32;
-                
-                buffer.push(value.try_into().unwrap());
-            }
-        }
-
-        buffer
-    }
-
-    fn subtract(&self, heightmap: &Heightmap) -> Result<Heightmap, HeightmapError> {
-        let mut data: Vec<Vec<f32>> = Vec::new();
-        
-        if !(self.width == heightmap.width && self.height == heightmap.height) {
-            return Err(HeightmapError::MismatchingSize)
-        }
-
-        for i in 0..self.width {
-            let mut row = Vec::new();
-            for j in 0..self.height {
-                let value = self.data[i][j] - heightmap.data[i][j] + heightmap.depth;
-                row.push(value);
-            }
-            data.push(row);
-        }
-
-        let diff = Heightmap::new(data, self.width, self.height, self.depth + heightmap.depth);
-        Ok(diff)
-    }
-
-}
-
-fn heightmap_to_image(heightmap: &Heightmap, filename: &str) -> image::ImageResult<()> {
+fn heightmap_to_image(heightmap: &heightmap::Heightmap, filename: &str) -> image::ImageResult<()> {
     let buffer = heightmap.to_u8();
 
     // Save the buffer as "image.png"
